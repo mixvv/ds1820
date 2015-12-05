ebea13526087596f9a3b10d104e4244ed38b6269
Valery Mikheika <mixvv@mail.ru>
2015-12-05 20:00:56 +0300

    Initial commit

---
+++ ds1820/main.c
@@ -0,0 +1,18 @@
+/*
+ * ds1820.c
+ *
+ * Created: 05.12.2015 19:59:26
+ * Author : Valery
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
